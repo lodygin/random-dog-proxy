69e993b7a55db45d3f50fd5cb0109960dcc4e02c
lodygin <lodigin.alexander@gmail.com>
2023-05-01 18:29:35 +0300

    *

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// Enable cors
+app.use(cors());
+
 // Router
 app.use('/api', require('./routes'));
-
-// Enable cors
-app.use(cors({ origin: false }));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
